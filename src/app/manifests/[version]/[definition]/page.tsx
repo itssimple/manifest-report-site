19aaa78c3ad77c8c3409eac62a337732c7127ddc
NoLifeKing <github-things@nolifeking85.tv>
2025-02-18 20:37:54 +0100

    chore: Some more metadata

--- src/app/manifests/[version]/[definition]/page.tsx
+++ src/app/manifests/[version]/[definition]/page.tsx
@@ -1,6 +1,7 @@
 import { cleanDefinitionName } from "@/app/shared-methods";
 import { DiffFile, ManifestListItem } from "@/types/manifestListTypes";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { Metadata } from "next";
 
 const s3 = new S3Client({
 	region: "manifest-report",
@@ -28,6 +29,44 @@ export async function generateStaticParams() {
 			version: post.VersionId,
 			definition: cleanDefinitionName(file.FileName)
 		})));
+}
+
+function getManifestFromList(version: string) : ManifestListItem {
+	return manifestList.find((post: ManifestListItem) => post.VersionId === version);
+}
+
+function ogDescription(manifest: ManifestListItem) {
+	let totalChangedFiles = 0;
+	let totalChanges = 0;
+	manifest.DiffFiles.forEach((file) => {
+		totalChangedFiles++;
+		totalChanges += file.Added + file.Modified + file.Unclassified + file.Removed + file.Reclassified;
+	});
+	return `Manifest version ${manifest.Version} was discovered on ${manifest.DiscoverDate_UTC}
+	And has ${totalChangedFiles} files changed with a total of ${totalChanges} changes`;
+}
+
+export async function generateMetadata(
+	{
+		params,
+	}: {
+		params: Promise<{ version: string, definition: string }>;
+	},
+	//parent: ResolvingMetadata
+): Promise<Metadata> {
+	const { version, definition } = await params;
+
+	const manifest = getManifestFromList(version);
+	return {
+		title: `Manifest version ${version}, definition ${definition} - Manifest.report`,
+		description: `Changes for definition ${definition} in version ${version}`,
+		openGraph: {
+			title: `Destiny 2 Manifest ${manifest.Version}/${definition} information`,
+			releaseDate: getManifestFromList(version).DiscoverDate_UTC,
+			url: `https://site.manifest.report/manifests/${version}/${definition}`,
+			description: ogDescription(manifest)
+		}
+	}
 }
 
 export default async function ManifestVersion({
